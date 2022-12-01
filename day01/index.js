33ee17b1eec9c61d19a1e5622961d2c29a0fdb7d
Nathalie Wassgren <nathalie.wassgren@svt.se>
2022-12-01 16:43:11 +0100

    day one

--- day01/index.js
+++ day01/index.js
@@ -1,25 +1,30 @@
 const fs = require('fs')
 
 function inputDataLinesIntegers(filename="input.txt") {
-    return fs.readFileSync(filename).toString().trim().split("\n").map((x)=>parseInt(x))
+    return fs.readFileSync(filename).toString().trim();
 }
 
-function getSolutionPart1() {
-    return inputDataLinesIntegers().reduce((x,y)=>x+y)
+function getSolutionPart1(input) {
+    const chunks = input.split('\n\n').map(c => c.split('\n'));
+    const sum = chunks.map(c => c.reduce((a, b) => parseInt(a) + parseInt(b), 0));
+    return sum.reduce((a, b) => Math.max(a, b), -Infinity);
 }
 
-function getSolutionPart2() {
-    return inputDataLinesIntegers().reduce((x,y)=>x*y)
+function getSolutionPart2(input) {
+    const chunks = input.split('\n\n').map(c => c.split('\n'));
+    const sum = chunks.map(c => c.reduce((a, b) => parseInt(a) + parseInt(b), 0));
+    const top = sum.sort((a, b) => a - b).reverse().slice(0,3);
+    return top.reduce((partialSum, a) => partialSum + a, 0);
 }
 
-console.log("Javascript")
 const part = process.env.part || "part1"
 
+const data = inputDataLinesIntegers();
 if (part === "part1")
-    console.log(getSolutionPart1())
+    console.log(getSolutionPart1(data))
 else
-    console.log(getSolutionPart2())
+    console.log(getSolutionPart2(data))
 
 module.exports = {
-    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
+    getSolutionPart1, getSolutionPart2
 }
--- day01/test/index.test.js
+++ day01/test/index.test.js
@@ -1,9 +1,34 @@
 const {getSolutionPart1, getSolutionPart2} = require('../index')
 
 test('part1', ()=>{
-    expect(getSolutionPart1()).toBe(1379)
+    const input = '1\n' +
+        '20\n' +
+        '\n' +
+        '10\n' +
+        '\n' +
+        '1\n' +
+        '1\n' +
+        '1';
+    expect(getSolutionPart1(input)).toBe(21)
 })
 
 test('part2', ()=>{
-    expect(getSolutionPart2()).toBe(56154)
+    const input = '1\n' +
+        '2\n' +
+        '\n' +
+        '20\n' +
+        '20\n' +
+        '\n' +
+        '10\n' +
+        '20\n' +
+        '\n' +
+        '10\n' +
+        '40\n' +
+        '\n' +
+        '10\n' +
+        '\n' +
+        '1\n' +
+        '1\n' +
+        '1';
+    expect(getSolutionPart2(input)).toBe(120)
 })
